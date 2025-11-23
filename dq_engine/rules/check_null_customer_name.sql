de193f7ddf4f16996bc6b4c01bcd7b169a90c591
Raiyankarim <karimrumi97@gmail.com>
2025-11-24 00:05:58 +0530

    added percent logic rebased

--- dq_engine/rules/check_null_customer_name.sql
+++ dq_engine/rules/check_null_customer_name.sql
@@ -1,7 +1,9 @@
 -- Rule: Check for NULL customer_name values
 -- Purpose: Ensure customer_name is always populated for analytics
 
-SELECT COUNT(*) AS null_customer_name_count
+SELECT
+    COUNT(*) AS null_customer_name_count,
+    COUNT(*)*1.0/(SELECT COUNT(*)  FROM customers) AS null_customer_name_percentage
 FROM customers
 WHERE customer_name IS NULL
 and is_active = 1;
